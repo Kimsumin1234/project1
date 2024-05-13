276a022c1bcc4d2abe034b12cdf041b2ac01096c
sumin <whshaks14@gmail.com>
2024-05-13 18:20:02 +0900

    회원정보 수정 페이지 작성

--- src/main/java/com/example/project1/controller/MemberController.java
+++ src/main/java/com/example/project1/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.example.project1.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,9 +20,17 @@ public class MemberController {
         log.info("로그인 페이지 요청");
     }
 
+    @PreAuthorize("hasRole('MEMBER')")
     @GetMapping("/profile")
     public void getProfile() {
         log.info("마이 페이지 요청");
+    }
+
+    @PreAuthorize("hasRole('MEMBER')")
+    @GetMapping("/edit")
+    public String getEditProfile() {
+        log.info("회원정보수정 페이지 요청");
+        return "/member/edit-profile";
     }
 
 }
