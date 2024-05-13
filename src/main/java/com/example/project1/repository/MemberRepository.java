0eac349c78c91d3bdbeb3578fe9f5ba169f154df
sumin <whshaks14@gmail.com>
2024-05-13 15:03:45 +0900

    MemberRepository findByEmail 메소드 작성

--- src/main/java/com/example/project1/repository/MemberRepository.java
+++ src/main/java/com/example/project1/repository/MemberRepository.java
@@ -1,9 +1,11 @@
 package com.example.project1.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.project1.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    Optional<Member> findByEmail(String email);
 }
