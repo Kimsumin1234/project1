b3850547f982e276679fe04caa840d051a54d0d5
kiuugi <jeung9963@gmail.com>
2024-05-13 19:44:36 +0900

    주석해제

--- src/main/java/com/example/project1/repository/ReviewReplyRepository.java
+++ src/main/java/com/example/project1/repository/ReviewReplyRepository.java
@@ -9,6 +9,7 @@ import com.example.project1.entity.ReviewReply;
 
 public interface ReviewReplyRepository extends JpaRepository<ReviewReply, Long> {
 
+    // 에러가 안뜨는 것부터 주석 해제
     List<ReviewReply> getReviewRepliesByReviewOrderByReplyNo(Review review);
 
 }
