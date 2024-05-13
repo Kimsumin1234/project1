b35c96840bf0a8cb2a3e29b413ad7b0f9ec0b40c
sumin <whshaks14@gmail.com>
2024-05-13 15:26:10 +0900

    AdoptUserService dtoToEntity, entityToDto 메소드 작성

---
+++ src/main/java/com/example/project1/service/AdoptUserService.java
@@ -0,0 +1,32 @@
+package com.example.project1.service;
+
+import com.example.project1.dto.MemberDto;
+import com.example.project1.entity.Member;
+
+public interface AdoptUserService {
+
+    // dto => entity
+    public default Member dtoToEntity(MemberDto memberDto) {
+        return Member.builder()
+                .email(memberDto.getEmail())
+                .phone(memberDto.getPhone())
+                .nickname(memberDto.getNickname())
+                .password(memberDto.getPassword())
+                .role(memberDto.getRole())
+                .build();
+    }
+
+    // entity => dto
+    public default MemberDto entityToDto(Member member) {
+        return MemberDto.builder()
+                .mid(member.getMid())
+                .email(member.getEmail())
+                .phone(member.getPhone())
+                .nickname(member.getNickname())
+                .password(member.getPassword())
+                .role(member.getRole())
+                .createdDate(member.getCreatedDate())
+                .lastModifiedDate(member.getLastModifiedDate())
+                .build();
+    }
+}
