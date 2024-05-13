58cdfafc15e7bfd7c4519ecec42ba2668481b13f
sumin <whshaks14@gmail.com>
2024-05-13 16:07:53 +0900

    마이페이지 로그인 작업

---
+++ src/main/java/com/example/project1/service/AdoptUserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.project1.service;
+
+import java.util.Optional;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.project1.dto.AuthMemberDto;
+import com.example.project1.entity.Member;
+import com.example.project1.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@RequiredArgsConstructor
+@Service
+public class AdoptUserServiceImpl implements UserDetailsService, AdoptUserService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 로그인 메소드
+        log.info("로그인 요청 {}", username);
+
+        Optional<Member> result = memberRepository.findByEmail(username);
+
+        if (!result.isPresent()) {
+            throw new UsernameNotFoundException("Check Email");
+        }
+        Member member = result.get();
+
+        return new AuthMemberDto(entityToDto(member));
+    }
+
+}
