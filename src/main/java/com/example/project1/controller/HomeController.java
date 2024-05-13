e24ef08c90fc22b7a74317868f5510fe3c32ff14
sumin <whshaks14@gmail.com>
2024-05-13 16:27:33 +0900

    HomeController @GetMapping("/auth") 작성

--- src/main/java/com/example/project1/controller/HomeController.java
+++ src/main/java/com/example/project1/controller/HomeController.java
@@ -1,10 +1,15 @@
 package com.example.project1.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Log4j2
@@ -14,6 +19,17 @@ public class HomeController {
     public String getHome() {
         log.info("Home 요청");
         return "index";
+    }
+
+    @PreAuthorize("permitAll()")
+    @ResponseBody
+    @GetMapping("/auth")
+    public Authentication getAuthenticationInfo() {
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+
+        return authentication;
     }
 
 }
