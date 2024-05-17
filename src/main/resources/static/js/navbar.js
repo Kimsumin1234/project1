e4ef353466cd356b325b1939875d3466b1d3ee52
sumin <whshaks14@gmail.com>
2024-05-17 17:57:56 +0900

    nav-item active 활성화

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,9 +1,37 @@
-const navItem = document.querySelectorAll(".nav-item");
-console.log(navItem);
+// const navItem = document.querySelectorAll(".nav-item");
+// console.log(navItem);
 
-navItem.forEach((li) => {
-  console.log(li);
-  li.addEventListener("click", (e) => {
-    console.log(e.target);
-  });
-});
+// navItem.forEach((li) => {
+//   // console.log(li);
+//   li.addEventListener("click", (e) => {
+//     console.log("current ", e.target);
+//   });
+// });
+
+const navbars = document.querySelector(".navbar-collapse .navbar-nav");
+const li = document.querySelectorAll(".navbar-collapse .navbar-nav .nav-item");
+
+// navbars.addEventListener("click", (event) => {
+//   console.log("current ", event.target);
+//   console.log(li);
+//   li.forEach((item) => {
+//     item.classList.add("active");
+//   });
+
+//   const target = event.target;
+
+//   // document.querySelector().parentElement
+
+//   console.log(target.parentElement);
+//   // target.parentElement.classList.add("active");
+
+//   // event.stopPropagation();
+// });
+
+// lis.forEach((li) => {
+//   li.addEventListener("click", (event) => {
+//     console.log(event.target);
+
+//     event.stopPropagation();
+//   });
+// });
