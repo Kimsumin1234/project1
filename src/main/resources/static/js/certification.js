502b8063fce1965d93aa6d52246384b7e30bbdff
sumin <whshaks14@gmail.com>
2024-05-21 18:03:43 +0900

    인증번호 검증 수정

--- src/main/resources/static/js/certification.js
+++ src/main/resources/static/js/certification.js
@@ -16,7 +16,24 @@ console.log(phone);
 //   return regex.test(pNum);
 // }
 
+let time = 180;
+let timer = setInterval(function () {
+  if (time > 0) {
+    // >= 0 으로하면 -1까지 출력된다.
+    time = time - 1; // 여기서 빼줘야 3분에서 3분 또 출력되지 않고, 바로 2분 59초로 넘어간다.
+    let min = Math.floor(time / 60);
+    let sec = String(time % 60).padStart(2, "0");
+    timeMin.innerText = min;
+    timeSec.innerText = sec;
+    // time = time - 1
+  } else {
+    certification.style.visibility = "hidden";
+  }
+}, 1000);
+clearInterval(timer);
+
 addon1.addEventListener("click", () => {
+  clearInterval(timer);
   const formData = new FormData();
   formData.append("phone", phone.value);
   //   console.log(phone.value);
@@ -37,10 +54,8 @@ addon1.addEventListener("click", () => {
         phoneCheck.innerHTML = data.phone;
       } else {
         phoneCheck.innerHTML = "";
-        certification.style.visibility = "visible";
-
-        let time = 180;
-        setInterval(function () {
+        time = 180;
+        timer = setInterval(function () {
           if (time > 0) {
             // >= 0 으로하면 -1까지 출력된다.
             time = time - 1; // 여기서 빼줘야 3분에서 3분 또 출력되지 않고, 바로 2분 59초로 넘어간다.
@@ -53,6 +68,7 @@ addon1.addEventListener("click", () => {
             certification.style.visibility = "hidden";
           }
         }, 1000);
+        certification.style.visibility = "visible";
       }
     });
 });
