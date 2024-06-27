9c111eb697813313b4ba7d7ceccab2943095e65d
minju <ngit0001@gmail.com>
2024-06-27 16:24:43 +0900

    네이버 지도 정보창 띄우기

--- src/main/resources/static/js/naverMap.js
+++ src/main/resources/static/js/naverMap.js
@@ -1,75 +1,62 @@
-// parseFloat → parseDouble(없는건가?)
-// Flaot 소수점 7번째까지 가능
-const latitude = parseFloat(document.querySelector("#latitude").value);
-const longitude = parseFloat(document.querySelector("#longitude").value);
+document.addEventListener("DOMContentLoaded", function () {
+  var mapDataElement = document.getElementById("map-data");
+  var careNm = mapDataElement.getAttribute("data-care-nm");
+  var careAddr = mapDataElement.getAttribute("data-care-addr");
 
-console.log(parseFloat(latitude));
-console.log(parseFloat(longitude));
+  var contentString = [
+    '<div class="desc">',
+    "<h5>" + careNm + "</h5>",
+    "<p>주소 : " + careAddr + "</p>",
+    "<p>★ 입양 문의는 해당보호소로 문의해주시길 바랍니다.</p>",
+    "</div>",
+  ].join("");
 
-// 기본 마커 형태 코드
-// var map = new naver.maps.Map("map", {
-//   center: new naver.maps.LatLng(latitude, longitude),
-//   zoomControl: true, //줌 컨트롤의 표시 여부
-//   zoom: 18,
-// });
+  var latitude = parseFloat(document.querySelector("#latitude").value);
+  var longitude = parseFloat(document.querySelector("#longitude").value);
 
-// var marker = new naver.maps.Marker({
-//   position: new naver.maps.LatLng(latitude, longitude),
-//   map: map,
-// });
+  console.log(parseFloat(latitude));
+  console.log(parseFloat(longitude));
 
-// 이미지 마커 넣는 코드
-var position = new naver.maps.LatLng(latitude, longitude);
+  var position = new naver.maps.LatLng(latitude, longitude);
 
-var map = new naver.maps.Map("map", {
-  center: position,
-  zoomControl: true, //줌 컨트롤의 표시 여부
-  zoom: 18,
-});
+  var map = new naver.maps.Map("map", {
+    center: position,
+    zoomControl: true,
+    zoom: 17,
+  });
 
-var markerOptions = {
-  position: position,
-  map: map,
-  icon: {
-    url: "/images/dog50.png",
-    size: new naver.maps.Size(50, 50),
-    origin: new naver.maps.Point(0, 0),
-    anchor: new naver.maps.Point(16, 32),
-  },
-};
+  var markerOptions = {
+    position: position,
+    map: map,
+    icon: {
+      url: "/images/dog50.png",
+      size: new naver.maps.Size(50, 50),
+      origin: new naver.maps.Point(0, 0),
+      anchor: new naver.maps.Point(16, 32),
+    },
+  };
 
-var marker = new naver.maps.Marker(markerOptions);
+  var marker = new naver.maps.Marker(markerOptions);
 
-var marker = new naver.maps.Marker(markerOptions);
+  var infowindow = new naver.maps.InfoWindow({
+    content: contentString,
+    maxWidth: 400,
+    backgroundColor: "#FDFDFD",
+    borderColor: "#CFE2F3",
+    borderWidth: 5,
+    anchorSize: new naver.maps.Size(30, 30),
+    anchorSkew: true,
+    anchorColor: "#FDFDFD",
+    pixelOffset: new naver.maps.Point(20, -20),
+  });
 
-// min/max 줌 레벨
-$("#min-max-zoom").on("click", function (e) {
-  e.preventDefault();
+  naver.maps.Event.addListener(marker, "click", function (e) {
+    if (infowindow.getMap()) {
+      infowindow.close();
+    } else {
+      infowindow.open(map, marker);
+    }
+  });
 
-  if (map.getOptions("minZoom") === 10) {
-    map.setOptions({
-      minZoom: 7,
-      maxZoom: 21,
-    });
-    $(this).val(this.name + ": 7 ~ 21");
-  } else {
-    map.setOptions({
-      minZoom: 10,
-      maxZoom: 21,
-    });
-    $(this).val(this.name + ": 10 ~ 21");
-  }
-});
-
-// 관성 드래깅 옵션
-$("#kinetic").on("click", function (e) {
-  e.preventDefault();
-
-  if (map.getOptions("disableKineticPan")) {
-    map.setOptions("disableKineticPan", false); //관성 드래깅 켜기
-    $(this).addClass("control-on");
-  } else {
-    map.setOptions("disableKineticPan", true); //관성 드래깅 끄기
-    $(this).removeClass("control-on");
-  }
+  infowindow.open(map, marker);
 });
