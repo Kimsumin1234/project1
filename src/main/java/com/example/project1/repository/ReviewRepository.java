0d7dc1b49a52163e12282458e3659b848b36c8d4
kiuugi <jeung9963@gmail.com>
2024-07-10 17:05:06 +0900

    좋아요한 글 뜨도록 수정

--- src/main/java/com/example/project1/repository/ReviewRepository.java
+++ src/main/java/com/example/project1/repository/ReviewRepository.java
@@ -21,7 +21,7 @@ public interface ReviewRepository
     @Query("select r from ReviewReply r where r.review.rno=:rno Order By r.replyNo")
     List<ReviewReply> getReviewReplies(Long rno);
 
-    @Query(value = "SELECT r1_0.rno, r1_0.created_date, r1_0.last_modified_date, r1_0.text, r1_0.title, r1_0.view_count, r1_0.writer_mid FROM review r1_0 LEFT JOIN heart h1_0 ON r1_0.rno = h1_0.review_rno WHERE h1_0.hno =?1", nativeQuery = true)
+    @Query(value = "SELECT r1_0.rno, r1_0.created_date, r1_0.last_modified_date, r1_0.text, r1_0.title, r1_0.view_count, r1_0.writer_mid, r1_0.heart_count FROM review r1_0 LEFT JOIN heart h1_0 ON r1_0.rno = h1_0.review_rno WHERE h1_0.hno =?1", nativeQuery = true)
     Review findByHeart(Long hno);
 
     @Modifying
